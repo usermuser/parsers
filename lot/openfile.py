dc8b82d321eda07941c9381056989840bbfdd5b8
usermuser <lwrdrstv@gmail.com>
2017-07-26 16:39:25 +0500

    fdkfjl

--- lot/openfile.py
+++ lot/openfile.py
@@ -35,23 +35,13 @@ soup = BeautifulSoup(s,'html.parser')
 tr_class = soup.find('tr',{'class': 'numbers'}).select("td")
 print len(tr_class)
 print (type(tr_class))
+print (tr_class)
 
-#ss='\n'.join(str(tr_class))
-#print(ss)
-#print(tr_class[0])
-
-for i in range((len(tr_class)-1)):
-	if str(tr_class[i])=='<td></td>':
-		del tr_class[i]
-	else: print(tr_class[i])
-
-
-
-'''
-tr_class.remove('<td></td>')
-print(tr_class)
-ss=str(tr_class[0])
-print(ss)
-print(type(ss))
-'''
+l=[]
+r=range(len(tr_class))
+print r
+for i in range(len(tr_class)-1):
+	if str(tr_class[i]) == '<td></td>': 
+		print(tr_class[i]) 
+		#del(tr_class[i])
 
