579a135021ce44119aa9eaefd4a0d8f95f80371d
usermuser <lwrdrstv@gmail.com>
2017-07-27 12:07:31 +0500

    converted tr_class elements to string
     and cuted tds

--- lot/openfile.py
+++ lot/openfile.py
@@ -1,13 +1,15 @@
 #https://habrahabr.ru/post/280238/
 #http://www.stoloto.ru/ruslotto/game
+#-*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import lxml
-import requests
+#import lxml
+#import requests
 
 
 try:
-    f=open('/home/user/work/parsers/lot/raw3.html','r')
+    #f=open('/home/user/work/parsers/lot/raw3.html','r')
+    f=open('raw3.html','r', encoding='utf-8')
 except IOError:
     sys.exit("Could not open file!")
 
@@ -33,15 +35,15 @@ soup = BeautifulSoup(s,'html.parser')
 
 #ticket_list = soup.find('div', {'class': 'bingo_ticket ruslotto'})
 tr_class = soup.find('tr',{'class': 'numbers'}).select("td")
-print len(tr_class)
+print (len(tr_class))
 print (type(tr_class))
 print (tr_class)
-
-l=[]
-r=range(len(tr_class))
-print r
-for i in range(len(tr_class)-1):
-	if str(tr_class[i]) == '<td></td>': 
-		print(tr_class[i]) 
-		#del(tr_class[i])
-
+print(type(tr_class[0]))
+print(tr_class[0])
+print('======================')
+res=[]
+for item in tr_class:
+    x=str(item)[4:-5]
+    #res.append(x[4:-5])
+    if x!='': res.append(x)
+print(res)
